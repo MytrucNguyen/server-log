2aee674620347bde7484bcbeedb89b8146547c10
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-05-13 12:56:10 -0700

    Updated Server Log Header

---
+++ src/components/ServerLog/ServerLogBody/index.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+export default function ServerLogBody(){
+    return(
+        <div className="serverLogBody">
+            <div className='serverBodyWrapper'>
+                <div className='topOfServerBody'>
+                    <h4>>_ Logs</h4>
+                    <hr/>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ServerLog/index.jsx
@@ -0,0 +1,11 @@
+import ServerLogHeader from "../ServerLogHeader";
+import ServerLogBody from "./ServerLogBody";
+
+export default function ServerLog(){
+    return (
+        <>
+            <ServerLogHeader />
+            <ServerLogBody />
+        </>
+    )
+}
